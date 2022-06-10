4b4721ce5c92b3b12d54b749cc2234bb1f4ae232
Elizaveta-Abramkina <88145988+Elizaveta-Abramkina@users.noreply.github.com>
2022-06-10 13:21:09 +0300

    'Bubbles-animation'

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import React, { useState} from 'react';
+import React, {useState} from 'react';
 import './App.css'
+import './backgrounAnimations.css'
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import BrendPage from './pages/BrendPage/BrendPage';
 import Home from "./pages/Home/Home";
 import Description from "./pages/Description/Description";
-/* eslint-disable */
 
 
 const App = () => {
   const [touchStart, setTouchStart] = useState<number | null>(null)
   const [touchEnd, setTouchEnd] = useState<number | null>(null)
   const [value, setValue] = useState<number>(0)
-  const [swipe, setSwipe] = useState<'left-swipe' | 'right-swipe'| null>(null)
+  const [swipe, setSwipe] = useState<'left-swipe' | 'right-swipe' | null>(null)
 
-  const rightSwipe=()=>{
+  const rightSwipe = () => {
     setSwipe('right-swipe')
     setValue(value - 1)
   }
-  const leftSwipe = ()=>{
+  const leftSwipe = () => {
     setSwipe('left-swipe')
-    setValue(value + 1 )
+    setValue(value + 1)
   }
 
-  const goHome = ()=>{
+  const goHome = () => {
     console.log('home')
     setValue(0)
   }
 
   const position = ['left', 'center', 'right']
-  const element = [<Home functionSwipe={leftSwipe}/>, <Description/>, <BrendPage/>]
+  const element = [<Home functionSwipe={leftSwipe} key='home'/>, <Description key='Description'/>,
+    <BrendPage key='Brend'/>]
   const style = {
     app: {backgroundPositionX: `${position[value]}`},
     appScroll: {animation: `${swipe} 0.5s`}
@@ -45,16 +46,16 @@ const App = () => {
   const onTouchMove = (e: React.TouchEvent<HTMLDivElement>) => setTouchEnd(e.targetTouches[0].pageX)
 
   const resetSwipe = () => {
-    setTimeout(()=>setSwipe(null), 500)
+    setTimeout(() => setSwipe(null), 500)
   }
 
   const onTouchEnd = () => {
     if (!touchStart || !touchEnd) return
-    const distance = touchStart! - touchEnd!
+    const distance = touchStart - touchEnd
     const isLeftSwipe = distance > minSwipeDistance
     const isRightSwipe = distance < -minSwipeDistance
 
-    if (isLeftSwipe && value <= 1 && value >=0){
+    if (isLeftSwipe && value <= 1 && value >= 0) {
       leftSwipe()
       resetSwipe()
     }
@@ -70,10 +71,10 @@ const App = () => {
     <div className='app' style={style.app} onTouchStart={onTouchStart} onTouchMove={onTouchMove}
          onTouchEnd={onTouchEnd}>
 
-        <div className='element-wrapper' style={style.appScroll}>
-          <Header functionHome={goHome}/>
-          {element[value]}
-        </div>
+      <div className='element-wrapper' style={style.appScroll}>
+        <Header functionHome={goHome}/>
+        {element[value]}
+      </div>
       <Footer/>
     </div>
 
--- src/pages/BrendPage/BrendPage.tsx
+++ src/pages/BrendPage/BrendPage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './BrendPage.css'
 import closeIcon from '../../assets/img/close.png'
 import bottleIcon from './../../assets/img/bottle.png'
@@ -14,69 +14,109 @@ interface IState {
   text: string
 }
 
+interface IBubbles {
+  backgroundPosition: string,
+  backgroundSize: string
+}
+
 const BrendPage = () => {
 
-  const state:IState = {
-    styleContent: 'brend-content',
-    styleBrend: 'brend',
-    text: 'ключевое сообщение'
-  }
+    const state: IState = {
+      styleContent: 'brend-content',
+      styleBrend: 'brend',
+      text: 'ключевое сообщение'
+    }
+    const babblesState: IBubbles = {
+      backgroundPosition: ' 23% 28%, 90% 50%, 65% 8%, 85% 80%, 0% 65%',
+      backgroundSize: '15% , 35% ,10%, 15%, 12%'
+    }
 
-  const [popUp, setPopUp] = useState<boolean>(false)
-  const [popUpStyle, setPopUpStyle] = useState(state)
+    const [popUp, setPopUp] = useState<boolean>(false)
+    const [popUpStyle, setPopUpStyle] = useState(state)
+    const [bubblesArr] = useState([[23, 28, 90, 50, 65, 8, 85, 80, 0, 65], [15, 35, 10, 15, 12]])
+    const [bubbles, setBubbles] = useState<IBubbles>(babblesState)
+
+    const random = () => {
+      return Math.floor(Math.random() * (5 - -5 + 1)) - 5;
+    }
 
 
+    const bubblesAnimation = () => {
+      const newValuesPosition = bubblesArr[0].map(el => el + random())
+      const newValuesSize = bubblesArr[1].map(el => el + random())
+      let positionStr = ''
+      let sizeStr = ''
+      newValuesPosition.forEach((el, index) => {
+        if (index % 2 !== 0 || index !== 0) {
+          positionStr += `${el}%, `
+        }
+        positionStr += `${el}% `
+      })
 
-  const openPopUp = () => {
-    setPopUp(!popUp)
-    setPopUpStyle({
-      styleContent: 'brend-content popup-content',
-      styleBrend: 'brend popup-brend',
-      text: 'преимущества'
-    })
-  }
+      newValuesSize.forEach((el) => {
+        sizeStr += `${el}%, `
+      })
 
-  const closePopup = ()=>{
-    setPopUp(!popUp)
-    setPopUpStyle(state)
-  }
+      const newState: IBubbles = {
+        backgroundPosition: positionStr,
+        backgroundSize: sizeStr,
+      }
+      setBubbles(newState)
+    }
 
-  const showPopUp = () => {
-    return !popUp ? (
-      <div className='brend-blocks'>
-        <TextBlock icon={plateIcon} text='Ehicula ipsum a arcu cursus vitae. Eu non diam
+    useEffect(()=>{setTimeout(bubblesAnimation, 3000)})
+
+
+    const openPopUp = () => {
+      setPopUp(!popUp)
+      setPopUpStyle({
+        styleContent: 'brend-content popup-content',
+        styleBrend: 'brend popup-brend',
+        text: 'преимущества'
+      })
+    }
+
+    const closePopup = () => {
+      setPopUp(!popUp)
+      setPopUpStyle(state)
+    }
+
+    const showPopUp = () => {
+      return !popUp ? (
+        <div className='brend-blocks'>
+          <TextBlock icon={plateIcon} text='Ehicula ipsum a arcu cursus vitae. Eu non diam
         phasellus vestibulum lorem sed
         risus ultricies'/>
-        <div>
-          <TextBlock icon={calendarIcon} text='A arcu cursus vitae'/>
-          <ButtonComponent text="Подробнее" onClick={openPopUp}/>
-        </div>
-      </div>
-    ) : (
-      <div className='popup'>
-        <Benefits/>
-      </div>
-    )
-  }
-
-
-  return (
-    <div className={popUpStyle.styleBrend}>
-      <img className='bottle-img' src={bottleIcon} alt='bottle'/>
-      <div className={popUpStyle.styleContent}>
-        <div className='brend-header'>
           <div>
-            <p className='simple-text'>{popUpStyle.text}</p>
-            <h1 className='brend-main-text'>brend <span className='accent-text'>XY</span></h1>
+            <TextBlock icon={calendarIcon} text='A arcu cursus vitae'/>
+            <ButtonComponent text="Подробнее" onClick={openPopUp}/>
           </div>
-          {popUp ? <img src={closeIcon} alt='close' className='close-btn' onClick={closePopup}/>: null}
         </div>
-        {showPopUp()}
+      ) : (
+        <div className='popup'>
+          <Benefits/>
+        </div>
+      )
+    }
+
+    return (
+      <div className={popUpStyle.styleBrend}>
+        <img className='bottle-img' src={bottleIcon} alt='bottle' style={bubbles}/>
+        <div className={popUpStyle.styleContent}>
+          <div className='brend-header'>
+            <div>
+              <p className='simple-text'>{popUpStyle.text}</p>
+              <h1 className='brend-main-text'>brend <span className='accent-text'>XY</span></h1>
+            </div>
+            {popUp ? <img src={closeIcon} alt='close' className='close-btn' onClick={closePopup}/> : null}
+          </div>
+          {showPopUp()}
+        </div>
       </div>
 
-    </div>
-
-  );
-};
+    );
+  }
+;
 
 export default BrendPage;
+
